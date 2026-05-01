a7a65631382bb036a91a272a4d05ca9f83a933b4
thiago <thiagoemanul260@gmail.com>
2026-05-01 16:56:48 -0300

    dia de melhoria no codigo, estou aprendendo bastante sobre logicas.

--- src/DoZero.java
+++ src/DoZero.java
@@ -3,55 +3,84 @@ import java.util.Scanner;
 
 public class DoZero {
     static void main(String[] args) {
+        //lembrar de ativar a String e args ja que eu uso da abreviação psvm
         System.out.println("===============");
         System.out.println("Seja bem vindo");
         System.out.println("Me diga o Valor do Produto");
 
         Scanner scanner = new Scanner(System.in);
-        double CalculoNovo = scanner.nextDouble();
+        double calculoNovo = scanner.nextDouble();
+
+        //denominei a variavel double (numeros decimais)
 
         System.out.println("o Valor é de "
-        + CalculoNovo + " R$");
+                + calculoNovo + " R$");
 
-        int[] DescontosP = {250, 350, 400, 500};
+        int[] DescontosP = {0, 250, 350, 400, 500};
 
-        double N = 3000;
-        double S = 5000;
-
-       boolean bool = false;
-       if (N > 2999) {
-           System.out.println("Voce acabou de ganhar um desconto, parabens." +
-                   "O seu desconto foi de " + DescontosP[2]);
-
-           else if ( S > 5001) {
-               System.out.println("Voce acabou de ganhar um desconto de " + DescontosP[3]);
-           }
-
-       }  else {
-        System.out.println("Não há desconto disponivel para este valor");}
-
-       double valorCDesconto = CalculoNovo - DescontosP[3];
-
-        System.out.println("O valor do produto com Desconto é de " + valorCDesconto);
-        System.out.println("Digite a quantidade de vezes que voce deseja parcelar");
-
-        double parcelasCartao = scanner.nextDouble();
+        //determinei valores para descontos fixos
 
 
-        System.out.println("O produto vai ser dividido em "
-        + parcelasCartao + " vezes");
+        if (calculoNovo > 10000) {
+            System.out.println("Voce acabou de ganhar um desconto, parabens. " +
+                    " O seu desconto foi de " + DescontosP[1]);
+            double valorCDesconto = calculoNovo - DescontosP[1];
+
+        }    //aprendi a melhorar a logica do meu codigo apenas observando o meu codigo
+        //consegui deixar bem mais pratico
+
+        else if (calculoNovo > 4500) {
+            System.out.println("Voce acabou de ganhar um desconto de " + DescontosP[3]);
+            double valorCDesconto1 = calculoNovo - DescontosP[3];
+        } else {
+            System.out.println("Não há desconto disponivel para este valor");
+            double valorCDesconto = calculoNovo - DescontosP[0];
+        }
+            //estou tentando entender como posso conseguir rodar o codigo sem ter que declarar a variavel valorCDesconto
+            //mas o progresso esta sendo feito
+
+        double valorCDesconto = calculoNovo - DescontosP[1];
+            //ainda nao sei como declarar descontos de formas aleatorias, imagino que tenha que declarar de forma
+            //especifica em cada valor com if, else e else if.
+
+            System.out.println("O valor do produto com Desconto é de " + valorCDesconto);
+            System.out.println("Digite a quantidade de vezes que voce deseja parcelar");
+
+            double parcelasCartao = scanner.nextDouble();
+            scanner.nextLine();
+
+            System.out.println("O produto vai ser dividido em "
+                    + parcelasCartao + " vezes");
 
 
-        double valorParcela = valorCDesconto / parcelasCartao;
+            double valorParcela = valorCDesconto / parcelasCartao;
 
-        System.out.println("O valor de cada parcela ficará "
-        + "R$" + valorParcela);
-
-        scanner.close();
-
-        System.out.println("Esse é o valor das parcelas, deseja realizar a compra?");
-        System.out.println("Obrigado");
+            System.out.println("O valor de cada parcela ficará "
+                    + "R$" + valorParcela);
 
 
+            System.out.println("Esse é o valor das parcelas, deseja realizar a compra?");
+
+            String word = scanner.nextLine();
+            System.out.println("Voce deseja fazer a compra?");
+
+            boolean palavra = word.equalsIgnoreCase ("Sim");
+
+
+            if (palavra) {
+                System.out.println("Insira o seu metodo para pagemtento "
+                        + (" Agradecemos por comprar conosco."));
+
+            } else {
+                System.out.println("Obrigado por entrar em contato conosco, desejamos que voce consgiga comprar o que voce deseja");
+            }
+
+            scanner.close();
+
+
+            System.out.println("Obrigado");
+
+
+        }
     }
-}
+
